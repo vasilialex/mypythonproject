bff343f31007075e872d7022a620aeb7625d420c
Vasiliy <vasilli@bk.ru>
2022-02-18 12:09:37 +0600

    first commit

---
+++ variables.py
@@ -0,0 +1,20 @@
+
+
+
+first_name = "vasiliy"
+last_name  = "pupkin"
+
+ege_of_born = 1986
+ege_of_now  = 2022
+
+eage_realy = (ege_of_now - ege_of_born)
+
+print (first_name + " " + last_name + " родился " + str(ege_of_born))
+
+print("сейчас" + str(ege_of_now))
+
+
+print ("сейчас ему " + str(eage_realy))
+
+
+
