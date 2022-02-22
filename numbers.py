931ce2dd4f1febaf59a532c47b517327a47c41da
Vasiliy <vasilli@bk.ru>
2022-02-22 15:42:45 +0600

    add lessons aboute numbers

---
+++ numbers.py
@@ -0,0 +1,9 @@
+
+
+
+
+num1 = 100
+num2 = 1
+p = 22/7
+
+print(num1+num2*p) 
---
+++ strings.py
@@ -0,0 +1,19 @@
+
+
+
+a = "    ...... vladimir vladivirovich     ....."
+print(a)
+
+print("----------------------------------")
+
+
+b = (a.strip())
+print(b)
+
+
+b = (b.strip("."))
+print(b.title())
+
+
+
+
