3a02df84c5364472298a5c5198bb07a5c18c020f
Vasiliy <vasilli@bk.ru>
2022-02-23 15:48:52 +0600

    add_while

---
+++ loops.py
@@ -0,0 +1,15 @@
+
+
+for my_age in range(0,100):
+    print("My age is" " " + str(my_age))
+    if my_age == 35:
+        print("You are right" + " " + str(my_age))
+        break
+
+
+x=9999
+while True:
+    print(x)
+    x = x + 1
+    if x == 10011:
+        break
