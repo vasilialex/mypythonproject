dbac8d98ded7a9deaf7a27c348d095d2564cdad4
Vasiliy <vasilli@bk.ru>
2022-02-23 15:55:43 +0600

    last_commit

--- variables.py
+++ variables.py
@@ -5,4 +5,5 @@ ege_of_now  = 2022
 eage_realy = (ege_of_now - ege_of_born)
 print (first_name + " " + last_name + " родился " + str(ege_of_born))
 print("сейчас" + str(ege_of_now))
+
 print ("сейчас ему " + str(eage_realy))
