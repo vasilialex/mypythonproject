aa43e18598e2c6d5f9c4105171db8be1b0269a25
Vasiliy <vasilli@bk.ru>
2022-02-28 11:52:20 +0600

    commit

---
+++ list2.py
@@ -0,0 +1,21 @@
+
+
+cars = [ 'wv', 'bmw', 'astin' ]
+
+
+for i in cars:
+    print(i.upper())
+
+
+number_list = list(range(1,10 + 1))
+
+print(number_list)
+
+for i in number_list:
+    print(i)
+
+#number_list.sort(reverse=True)
+
+#print max
+print("------------max_number_list-------")
+print(max(number_list))
