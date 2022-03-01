089cfb70213aa3ef6787de3c7dbb108743f24f39
Vasiliy <vasilli@bk.ru>
2022-03-01 16:41:15 +0600

    commit of dictionary

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+
+
+from re import L
+from sunau import AUDIO_FILE_ENCODING_ALAW_8
+
+
+my_hero = {
+    'age':      30,
+    'health':   100,
+    'pos_x':    145,
+    'posit_y':  25,
+    'color':    'blue',
+    'name':     'vasiliy',
+
+
+
+}
+
+
+
+print( str(my_hero['age'])  + "my_eage" + "  " + str(my_hero['health']) + "my_health")  
+
+my_hero['achive'] = 1 
+
+print(my_hero)
+
