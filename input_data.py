6fb69f9ecc26d72561cd8682194bb2723169343a
Vasiliy <vasilli@bk.ru>
2022-03-02 15:34:54 +0600

    mi_input_commit

--- input_data.py
+++ input_data.py
@@ -1,8 +1,37 @@
+#name = input("Enter your name: ")
+#print(name)
+
+#num1 = input("Enter your year of born: ")
+#num2 = input("Enter your current year: ")
+
+#age = int(num2) - int(num1)
+
+#print (str("Your age: ") + str(age))
 
 
+#password = ""
+#while True:
+#    password = input("enter your password")
+#    if password == 'vas':
+#        break
+#print("Welcome")
 
 
+mylist = []
+msg = ''
+#while msg != 'STOP':
+#    msg  = input("Enter your color")
+#    mylist.append(msg)
+    
 
-name = input("enter your name: ")
 
-print(name)
+while True:
+    msg = input("Input passwod")
+    if msg == 'STOP':
+        break
+print("password not correct")
+
+
+for i in mylist:
+    print(i)
+
